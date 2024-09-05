fbeb974bd73c2f404772a04008655d7650f1bf72
JSackey101 <jeffsackey101@gmail.com>
2024-09-05 14:58:54 +0100

    Added a draw method to the Cell class that draws the walls of the cell to the canvas if the Cell has the respective walls

--- maze.py
+++ maze.py
@@ -63,17 +63,34 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
+        self.x1 = x1 # top left corner
+        self.x2 = x2 # bottom right corner
+        self.y1 = y1 # top left corner
+        self.y2 = y2 # bottom right corner
         self.window = window
 
+    def draw(self):
+        top_left = Point(self.x1,self.y1)
+        top_right = Point(self.x2, self.y1)
+        bottom_right = Point(self.x2, self.y2)
+        bottom_left = Point(self.x1, self.y2)
+        if self.has_top_wall:
+            top_wall_line = Line(top_left, top_right)
+            self.window.draw_line(line=top_wall_line, fill_colour="black")
+        if self.has_bottom_wall:
+            bottom_wall_line = Line(bottom_left, bottom_right)
+            self.window.draw_line(line=bottom_wall_line, fill_colour="black")
+        if self.has_right_wall:
+            right_wall_line = Line(top_right, bottom_right)
+            self.window.draw_line(line=right_wall_line, fill_colour="black")
+        if self.has_left_wall:
+            left_wall_line = Line(top_left, bottom_left)
+            self.window.draw_line(line=left_wall_line, fill_colour="black")
 
 def main():
     win = Window(800, 600)
     first_point = Point(0,0)
-    second_point = Point(200,200)
+    second_point = Point(200,400)
     line = Line(first_point, second_point)
     win.draw_line(line=line, fill_colour="red")
     win.wait_for_close()
