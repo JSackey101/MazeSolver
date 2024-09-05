e4565cfffe093a49e97fdd84a333919db6c0258e
JSackey101 <jeffsackey101@gmail.com>
2024-09-05 13:19:16 +0100

    Changed wait_for_close method of the Window class to use a while loop to continue redrawing for now as it keeps the window open for testing Added a draw_line method to the Window class which takes a Line object and a string colour for its attributes and uses the draw method of the Line class to draw a line on the canvas Added a Point class that stores pixel coordinates as x and y attributes Added a Line class that takes and stores 2 Point class objects as its attributes Added a draw method to the Line class that takes a canvas object and a string colour for its attributes and uses this to draw a line on the canvas Changed the main function to test the new classes and methods

--- maze.py
+++ maze.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import Tk, BOTH, Canvas
 
 
@@ -20,11 +21,11 @@ class Window:
         self.width = width
         self.height = height
         self.root = Tk()  # Creates initial root widget using Tk() and sets it to root attribute
-        self.root.protocol("WM_DELETE_WINDOW", self.close())
         self.root.title("My Maze Solver Application")  # Sets the root attributes title
         self.canvas = Canvas(master=self.root)  # Creates a Canvas widget and sets it to the canvas attribute
         self.canvas.pack()  # Packs the canvas widget so it's ready to drawn
         self.running = False  # Represents whether the window is "running"
+        self.root.protocol("WM_DELETE_WINDOW", self.close())
 
     def redraw(self):
         self.root.update_idletasks()
@@ -32,19 +33,37 @@ class Window:
 
     def wait_for_close(self):
         self.running = True
-        if self.running:
+        while self.running:
             self.redraw()
 
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_colour):
+        line.draw(canvas=self.canvas, fill_colour=fill_colour)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
+class Line:
+    def __init__(self, point_one, point_two):
+        self.point_one = point_one
+        self.point_two = point_two
 
+    def draw(self, canvas, fill_colour):
+        canvas.create_line(self.point_one.x, self.point_one.y, self.point_two.x, self.point_two.y, fill=fill_colour,
+                           width=2)
 
 
 def main():
     win = Window(800, 600)
+    first_point = Point(0,0)
+    second_point = Point(200,200)
+    line = Line(first_point, second_point)
+    win.draw_line(line=line, fill_colour="red")
     win.wait_for_close()
 
 if __name__ == '__main__':
